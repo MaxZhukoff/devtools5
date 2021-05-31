1cf7abfe9594adad35cde9f633489ab02c5a2d3e
pavelspace <pavelkvetkin@gmail.com>
2021-05-31 21:25:33 +0600

    commit

---
+++ src/main/java/com/alpha/CustomDialogWrapper.java
@@ -0,0 +1,56 @@
+package com.alpha;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import org.jdesktop.swingx.*;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CustomDialogWrapper extends DialogWrapper {
+    JLabel labelTime;
+    JTextField textField;
+    JLabel labelMode;
+    JButton button1, button2;
+    TYPE type;
+    Project project;
+    public String input = "";
+
+    public CustomDialogWrapper(Project project) {
+        super(project);
+        init();
+        setTitle("Test DialogWrapper");
+        this.project = project;
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        var dialogPanel = new JPanel();
+        labelTime = new JLabel("After");
+        dialogPanel.add(labelTime);
+        textField = new JTextField();
+        dialogPanel.add(textField);
+        labelMode = new JXLabel("minute(s) remind me to");
+        dialogPanel.add(labelMode);
+        button1 = new JButton("Chill");
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                type = TYPE.CHILL;
+            }
+        });
+        dialogPanel.add(button1);
+        button2 = new JButton("Eat");
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                type = TYPE.EAT;
+            }
+        });
+        dialogPanel.add(button2);
+        return dialogPanel;
+    }
+}
