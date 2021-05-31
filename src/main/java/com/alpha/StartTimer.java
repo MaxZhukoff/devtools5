630935289e1f922436438941031f8ce1445f8c69
pavelspace <pavelkvetkin@gmail.com>
2021-05-31 21:13:09 +0600

    Adding Action

---
+++ src/main/java/com/alpha/StartTimer.java
@@ -0,0 +1,12 @@
+package com.alpha;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class StartTimer extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        // Create dialog window
+    }
+}
